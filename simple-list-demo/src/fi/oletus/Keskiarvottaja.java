c54d000a4ae37fb0c1756d21e02df422441b03a7
unknown <Jrjestelmnvalvoja@.(none)>
2011-07-30 14:09:48 +0300

    keskiarvottaja

---
+++ simple-list-demo/src/fi/oletus/Keskiarvottaja.java
@@ -0,0 +1,21 @@
+package fi.oletus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Keskiarvottaja {
+	
+	public static Integer keskiarvo(List<Integer> a)
+	{
+		// TODO: Laske listassa olevien lukujen keskiarvo
+		// ja palauta se.
+		return new Integer(0); // Tämä vain siksi, että kääntyisi
+	}
+	
+	public static void main(String[] args) {
+		List<Integer> a = new ArrayList<Integer>();
+		// TODO: Lisää listaan muutama Integer-alkio, esim. for-loopilla
+		// TODO: Kutsu keskiarvofunktiota ja tulosta keskiarvo
+		// TODO: Voit myös kokeilla Vector-luokkaa ArrayListin tilalle
+	}
+}
